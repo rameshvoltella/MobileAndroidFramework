0c5457bf8024eb53d65e671068922fc1b2c2c338
ENDAVA\lumihai <lucian.mihai@endava.com>
2017-07-24 16:08:47 +0300

    adding extra classes

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -6,12 +6,11 @@ import tests.TestPrimer;
 
 import java.net.MalformedURLException;
 
-/**
- * Created by lumihai on 6/29/2017.
- */
+
 public class Runner {
 
     public static void main(String[] args) throws MalformedURLException {
+
         MyLogger.log.setLevel(Level.INFO);
         try {
             DriverManager.createDriver();
