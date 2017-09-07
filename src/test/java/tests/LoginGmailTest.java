b801654441938af9f5c3308963a25b87d3e3a8ab
Jenkins Endava <jenkins@endava.com>
2017-09-07 16:42:41 +0300

    addapt driver manager for iOS also

--- src/test/java/tests/LoginGmailTest.java
+++ src/test/java/tests/LoginGmailTest.java
@@ -1,22 +1,22 @@
-package tests;
-
-import core.classicmethods.Gestures;
-import core.managers.TestManager;
-import org.junit.Before;
-import org.junit.Test;
-
-public class LoginGmailTest extends TestManager {
-
-    @Before
-    public void loginGmail() {
-//        mail.loginview.generalLogin("testingbriefcase@gmail.com", "testingbriefcase", 0);
-//        mail.loginview.generalLogin("emma.cluj@web.de", "test1234", 0);
-    }
-
-    @Test
-    public void sendGmailTest() {
-
-    }
-
-
-}
+//package tests;
+//
+//import core.classicmethods.Gestures;
+//import core.managers.TestManager;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//public class LoginGmailTest extends TestManager {
+//
+//    @Before
+//    public void loginGmail() {
+////        mail.loginview.generalLogin("testingbriefcase@gmail.com", "testingbriefcase", 0);
+////        mail.loginview.generalLogin("emma.cluj@web.de", "test1234", 0);
+//    }
+//
+//    @Test
+//    public void sendGmailTest() {
+//
+//    }
+//
+//
+//}
