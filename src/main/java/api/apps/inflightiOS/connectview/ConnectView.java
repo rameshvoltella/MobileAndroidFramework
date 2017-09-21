286abe3eded3644f55103df9ddb440bab7f76e01
Jenkins Endava <jenkins@endava.com>
2017-09-21 10:43:26 +0300

    addapt setup for jenkins integration

---
+++ src/main/java/api/apps/inflightiOS/connectview/ConnectView.java
@@ -0,0 +1,12 @@
+package api.apps.inflightiOS.connectview;
+
+import api.interfaces.Activity;
+
+public class ConnectView implements Activity {
+
+
+    @Override
+    public Object waitToLoad() {
+        return null;
+    }
+}
